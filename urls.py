2cd3e83fba07a10f93c3dd2fed5c1b70503554ee
Victor Lima <victor.lima@concretesolutions.com.br>
2011-04-29 12:33:18 -0300

    root url respondendo

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,6 +13,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', redirect_to, {'url': '/blog/'}),
     url(r'^blog/', include('blog.urls')),
     url(r'^oauth/', include('oauth.urls')),
 
