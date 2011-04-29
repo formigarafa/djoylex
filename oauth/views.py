5759b7fe5d8e8576ce1e9664243923244d8da26b
Victor Lima <victor.lima@concretesolutions.com.br>
2011-04-29 13:23:08 -0300

    login com layout de popup

--- oauth/views.py
+++ oauth/views.py
@@ -1,23 +1,22 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
 
 from oauth import Facebook
 from blog.models import User
 
 def step1(request):
-  return HttpResponseRedirect(Facebook().oauth_dialog_url())
+  return HttpResponseRedirect(Facebook().oauth_dialog_url()+"&display=popup")
 
 def step2(request):
   facebook_interface = Facebook()
   token = facebook_interface.token_from_request(request)
-#  operation_status = "sucesso codigo retornado: (%s).<br/>Token response: (%s)" % (facebook_interface.code, token)
+
   if token:
     user = User.from_facebook(facebook_interface)
     request.session['user_id'] = user.id
-#    profile = facebook_interface.profile()
-#    operation_status += "perfil (%s)" % (profile)
-#    operation_status += "user (%s)" % (user.username)
-  return HttpResponseRedirect('/blog/')
-  #return HttpResponse(operation_status)
+
+  return render_to_response('step2.html')
+
 def logout(request):
   del request.session['user_id']
   return HttpResponseRedirect('/blog/')
